9943175dcd6e17c59cc01d93940e37ea1953dbf5
Hamish Coleman <hamish@zot.org>
2018-03-03 11:41:58 +0800

    Add another module to load for qemu block devices

--- packages.d/_ALWAYS.fixup.add/init.d/10modules-qemu.sh
+++ packages.d/_ALWAYS.fixup.add/init.d/10modules-qemu.sh
@@ -5,5 +5,15 @@
 #
 # Copyright (C) 2017 Hamish Coleman <hamish@zot.org>
 
+# FIXME
+# - this list could get long and unmaintainable
+# - if we load the wrong module on a different system, we might get errors
+#   (which are both unsightly and making checking for real errors harder)
+#
+# TODO
+# - running with our own kernel will allow us to be sure to build-in the right
+#   drivers for block devices
+
 modprobe virtio_blk
+modprobe virtio_pci
 
